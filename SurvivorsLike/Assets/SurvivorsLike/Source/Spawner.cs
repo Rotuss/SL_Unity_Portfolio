312c6d4d3770424662e29472c9270ffb28283267
Rotus <12rotus31@gmail.com>
2024-07-23 13:07:22 +0900

    Rotus: Enemy 위치 랜덤 Spawn 작업 업로드

--- SurvivorsLike/Assets/SurvivorsLike/Source/Spawner.cs
+++ SurvivorsLike/Assets/SurvivorsLike/Source/Spawner.cs
@@ -1,9 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+    public Transform[] SpawnPoint;
+
+    float Timer;
+
+    private void Awake()
+    {
+        // GetComponentsInChildren는 자기 자신도 포함, 트랜스폼은 항상 포함되어 있기 때문
+        SpawnPoint = GetComponentsInChildren<Transform>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,11 +25,20 @@ public class Spawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // 스폰 테스트용
-        if(true == Input.GetButtonDown("Jump"))
+        Timer += Time.deltaTime;
+
+        if(0.5f < Timer)
         {
-            // float형과 다르게 int형은 마지막 값이 포함되지 않으므로 +1 해야함
-            GameManager.Instance.Pool.Get(Random.Range(0,2));
+            Timer = 0.0f;
+            Spawn();
         }
+
+    }
+
+    void Spawn()
+    {
+        GameObject EnemyObj = GameManager.Instance.Pool.Get(UnityEngine.Random.Range(0, GameManager.Instance.Pool.Prefabs.Length));
+        EnemyObj.transform.position = SpawnPoint[UnityEngine.Random.Range(1, SpawnPoint.Length)].position;
+
     }
 }
