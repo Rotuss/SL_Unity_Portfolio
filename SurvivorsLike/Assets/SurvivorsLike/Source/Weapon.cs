c5398f3e31ef91e23dfdb10e01a90f9dd73a4567
Rotus <12rotus31@gmail.com>
2024-07-24 14:24:53 +0900

    Rotus: 무기 관련 테스트 업로드
    -레벨에 따른 무기 업그레이드(무기 카운트 및 공격력)
    -Batch에서 기존에 사용 중인 무기가 있을 경우 재배치, 사용중인 것이 없다면 추가 배치

--- SurvivorsLike/Assets/SurvivorsLike/Source/Weapon.cs
+++ SurvivorsLike/Assets/SurvivorsLike/Source/Weapon.cs
@@ -27,6 +27,9 @@ public class Weapon : MonoBehaviour
             default:
                 break;
         }
+
+        // 레벨업 임시 테스트용
+        if (true == Input.GetButtonDown("Jump")) LevelUp(30);
     }
 
     #region Custom
@@ -44,13 +47,26 @@ public class Weapon : MonoBehaviour
         }
     }
 
+    public void LevelUp(float InDamage)
+    {
+        Damage = InDamage;
+        ++Count;
+
+        if (0 == ID) Batch();
+    }
+
     void Batch()
     {
         for(int i = 0; i < Count; ++i)
         {
-            Transform Bullet = GameManager.Instance.Pool.Get(PrefabID).transform;
+            // 기본 오브젝트 먼저 활용
+            // 모자라면 풀링에서 충당
+            Transform Bullet = transform.childCount > i? transform.GetChild(i) : GameManager.Instance.Pool.Get(PrefabID).transform;
+            
             Vector3 RotVec = Vector3.forward * 360 * i / Count;
             Bullet.parent = transform;                          // PoolManager 부모에서 현재 Weapon의 transform으로 변경
+            Bullet.localPosition = Vector3.zero;                // 로컬 포지션 초기화
+            Bullet.localRotation = Quaternion.identity;         // 로컬 로테이션 초기화
             Bullet.Rotate(RotVec);                              // 개수에 맞춰 회전
             Bullet.Translate(Bullet.up * 1.5f, Space.World);    // 위치
             Bullet.GetComponent<Bullet>().Init(Damage, -1);     // -1: 무한 관통, 근접 공격
