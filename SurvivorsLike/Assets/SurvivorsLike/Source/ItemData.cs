746c36dcb8639a8376c7fb4027c08c6624324313
Rotus <12rotus31@gmail.com>
2024-07-26 15:28:00 +0900

    Rotus: ItemData 생성
    -ScriptableObject를 활용하여 전반적 Item 정보 기반 생성
    -스크립트 커스텀 메뉴 생성으로 각 오브젝트 별 세부 정보 세팅

---
+++ SurvivorsLike/Assets/SurvivorsLike/Source/ItemData.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// [CreateAssetMenu] 커스텀 메뉴 생성하는 속성
+[CreateAssetMenu(fileName = "Item", menuName = "Scriptable Object/ItemData")]
+public class ItemData : ScriptableObject
+{
+    public enum ItemType { Melee, Range, Glove, Shoe, Heal }
+
+    [Header("Main Information")]
+    public ItemType     Type;
+    public Sprite       ItemIcon;
+    public string       ItemName;
+    public string       ItemDescription;
+    public int          ItemID;
+
+    [Header("Level Data")]
+    public float[]      Damages;
+    public float        BaseDamage;
+    public int[]        Counts;
+    public int          BaseCount;
+
+    [Header("Weapon Data")]
+    public GameObject   Projectile;
+}
