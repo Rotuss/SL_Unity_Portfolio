8e8343e60b9afc910e9568c7e6ac828e21addb14
Rotus <12rotus31@gmail.com>
2024-07-31 13:59:27 +0900

    Rotus: Ground 및 Enemy 재배치 관련 코드 수정

--- SurvivorsLike/Assets/SurvivorsLike/Source/Reposition.cs
+++ SurvivorsLike/Assets/SurvivorsLike/Source/Reposition.cs
@@ -21,30 +21,34 @@ public class Reposition : MonoBehaviour
         // 맵 위치
         Vector3 MapPosition = transform.position;
 
-        // 플레이어 방향
-        Vector3 PlayerDirection = GameManager.Instance.MainPlayer.InputVec;
-        float DirX = PlayerPosition.x - MapPosition.x;
-        float DirY = PlayerPosition.y - MapPosition.y;
-
-        float DiffPosX = Mathf.Abs(DirX);
-        float DiffPosY = Mathf.Abs(DirY);
-
-        DirX = 0 > DirX ? -1 : 1;
-        DirY = 0 > DirY ? -1 : 1;
-
         switch (transform.tag)
         {
             case "Ground":
+            {
+                // 플레이어 방향
+                float DirX = PlayerPosition.x - MapPosition.x;
+                float DirY = PlayerPosition.y - MapPosition.y;
+
+                float DiffPosX = Mathf.Abs(DirX);
+                float DiffPosY = Mathf.Abs(DirY);
+
+                DirX = 0 > DirX ? -1 : 1;
+                DirY = 0 > DirY ? -1 : 1;
+
                 if (DiffPosX > DiffPosY) transform.Translate(Vector3.right * DirX * 40);
                 else if (DiffPosX < DiffPosY) transform.Translate(Vector3.up * DirY * 40);
                 break;
+            }
             case "Enemy":
-                if(true == Col.enabled)
+            {
+                if (true == Col.enabled)
                 {
-                    // 플레이어 방향에 따라 맞은 편에서 등장
-                    transform.Translate(PlayerDirection * 25 + new Vector3(Random.Range(-3.0f, 3.0f), Random.Range(-3.0f, 3.0f), 0));
+                    // 거리차이로 위치 재배치
+                    Vector3 Dist = PlayerPosition - MapPosition;
+                    transform.Translate(Dist * 2 + new Vector3(Random.Range(-3.0f, 3.0f), Random.Range(-3.0f, 3.0f), 0));
                 }
                 break;
+            }
             default:
                 break;
         }
